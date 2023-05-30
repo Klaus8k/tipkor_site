fd19a64eae3055e6a8b6d595132439e56047caa0
Oleg <klaus888@yandex.ru>
2023-05-30 23:42:25 +0300

    add format m adn leaflets m

--- tipkor/poly/admin.py
+++ tipkor/poly/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from poly.models import Cards
+from poly.models import Cards, Leaflets, FormatsPoly
 
 # Register your models here.
-admin.site.register(Cards)
+admin.site.register([Cards, Leaflets, FormatsPoly])
--- tipkor/poly/models.py
+++ tipkor/poly/models.py
@@ -4,39 +4,58 @@ from django.db import models
 
 
 class MetaPoly(models.Model):
-    PAPER_130 = '130'
-    PAPER_170 = '170'
-    PAPER_300 = '300'
-    PAPER_CHOICE = [(PAPER_130, '130 г/м'),
-                    (PAPER_170, '170 г/м'),
-                    (PAPER_300, '300 г/м'),]
+    PAPER_130 = "130"
+    PAPER_170 = "170"
+    PAPER_300 = "300"
+    PAPER_CHOICE = [
+        (PAPER_130, "130 г/м"),
+        (PAPER_170, "170 г/м"),
+        (PAPER_300, "300 г/м"),
+    ]
 
-    DUPLEX = [(True, '4+4'), (False, '4+0')]
+    DUPLEX = [(True, "4+4"), (False, "4+0")]
 
-    x = models.IntegerField(help_text='Горизонтальный размер')
-    y = models.IntegerField(help_text='Вертикальный размер')
-    paper = models.CharField(choices=PAPER_CHOICE, max_length=20, help_text='Плотность бумаги')
-    pressrun = models.IntegerField(help_text='Тираж')
-    duplex = models.BooleanField(default=True, choices=DUPLEX, help_text='Дуплекс')
-    cost = models.IntegerField(null=True, help_text='Цена')
+    x = models.IntegerField(help_text="Горизонтальный размер")
+    y = models.IntegerField(help_text="Вертикальный размер")
+    paper = models.CharField(
+        choices=PAPER_CHOICE, max_length=20, help_text="Плотность бумаги"
+    )
+    pressrun = models.IntegerField(help_text="Тираж")
+    duplex = models.BooleanField(default=True, choices=DUPLEX, help_text="Дуплекс")
+    cost = models.IntegerField(null=True, help_text="Цена")
 
     def __str__(self) -> str:
-        duplex = '4+0'
+        duplex = "4+0"
         if self.duplex:
-            duplex = '4+4'
-        return '{}x{}, {}, {}, {}шт - {}руб'.format(self.x, self.y, self.paper, duplex, self.pressrun, self.cost)
-    
+            duplex = "4+4"
+        return "{}x{}, {}г/м, {}, {}шт - {}руб".format(
+            self.x, self.y, self.paper, duplex, self.pressrun, self.cost
+        )
+
     class Meta:
         abstract = True
 
+
 class Cards(MetaPoly):
-    x = models.IntegerField(default=90, help_text='Горизонтальный размер')
-    y = models.IntegerField(default=50, help_text='Вертикальный размер')
-    paper = models.CharField(choices=[('300', '300 г/м')], default='300', max_length=20, help_text='Плотность бумаги')
+    x = models.IntegerField(default=90, help_text="Горизонтальный размер")
+    y = models.IntegerField(default=50, help_text="Вертикальный размер")
+    paper = models.CharField(
+        choices=[("300", "300 г/м")],
+        default="300",
+        max_length=20,
+        help_text="Плотность бумаги",
+    )
 
+class FormatsPoly(models.Model):
+    format_paper = models.CharField(max_length=5)
+    x = models.IntegerField()
+    y = models.IntegerField()
+
+    def __str__(self):
+        return self.format_paper
     
+class Leaflets(MetaPoly):
+    format_choice = models.ForeignKey(FormatsPoly, on_delete=models.CASCADE)
 
-
-
-
-
+    def __str__(self):
+        return str(self.format_choice.format_paper) + super().__str__()[4:]
