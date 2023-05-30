dfcbbddbd2c2f3779e74554c6dfb6db5d3328688
Oleg Sokolov <klaus888@yandex.ru>
2023-05-30 18:26:59 +0300

    1

--- tipkor/poly/admin.py
+++ tipkor/poly/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from poly.models import Cards
 
 # Register your models here.
+admin.site.register(Cards)
--- tipkor/poly/models.py
+++ tipkor/poly/models.py
@@ -1,4 +1,34 @@
 from django.db import models
 
-# Create your models here.
-a = 933
+# Abstract class for poly
+
+
+class MetaPoly(models.Model):
+    PAPER_130 = 130
+    PAPER_170 = 170
+    PAPER_300 = 300
+    PAPER_CHOICE = [(PAPER_130, '130 г/м'),
+                    (PAPER_170, '170 г/м'),
+                    (PAPER_300, '300 г/м'),]
+
+    DUPLEX = [(True, '4+4'), (False, '4+0')]
+
+    x = models.IntegerField(default=90, help_text='Горизонтальный размер')
+    y = models.IntegerField(default=50, help_text='Вертикальный размер')
+    paper = models.CharField(choices=PAPER_CHOICE, max_length=20, help_text='Плотность бумаги')
+    pressrun = models.IntegerField(help_text='Тираж')
+    duplex = models.BooleanField(default=True, choices=DUPLEX, help_text='Дуплекс')
+
+    class Meta:
+        abstract = True
+
+class Cards(MetaPoly):
+    PAPER_CHOICE = [(300, '300 г/м')]
+
+
+    
+
+
+
+
+
