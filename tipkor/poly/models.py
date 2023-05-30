dc82d07f727b73fab333c8a362ae9e8b4c6bb08a
Oleg <klaus888@yandex.ru>
2023-05-30 22:27:53 +0300

    add card model

--- tipkor/poly/models.py
+++ tipkor/poly/models.py
@@ -4,27 +4,35 @@ from django.db import models
 
 
 class MetaPoly(models.Model):
-    PAPER_130 = 130
-    PAPER_170 = 170
-    PAPER_300 = 300
+    PAPER_130 = '130'
+    PAPER_170 = '170'
+    PAPER_300 = '300'
     PAPER_CHOICE = [(PAPER_130, '130 г/м'),
                     (PAPER_170, '170 г/м'),
                     (PAPER_300, '300 г/м'),]
 
     DUPLEX = [(True, '4+4'), (False, '4+0')]
 
-    x = models.IntegerField(default=90, help_text='Горизонтальный размер')
-    y = models.IntegerField(default=50, help_text='Вертикальный размер')
+    x = models.IntegerField(help_text='Горизонтальный размер')
+    y = models.IntegerField(help_text='Вертикальный размер')
     paper = models.CharField(choices=PAPER_CHOICE, max_length=20, help_text='Плотность бумаги')
     pressrun = models.IntegerField(help_text='Тираж')
     duplex = models.BooleanField(default=True, choices=DUPLEX, help_text='Дуплекс')
+    cost = models.IntegerField(null=True, help_text='Цена')
 
+    def __str__(self) -> str:
+        duplex = '4+0'
+        if self.duplex:
+            duplex = '4+4'
+        return '{}x{}, {}, {}, {}шт - {}руб'.format(self.x, self.y, self.paper, duplex, self.pressrun, self.cost)
+    
     class Meta:
         abstract = True
 
 class Cards(MetaPoly):
-    PAPER_CHOICE = [(300, '300 г/м')]
-
+    x = models.IntegerField(default=90, help_text='Горизонтальный размер')
+    y = models.IntegerField(default=50, help_text='Вертикальный размер')
+    paper = models.CharField(choices=[('300', '300 г/м')], default='300', max_length=20, help_text='Плотность бумаги')
 
     
 
