c9a823db90aeced9d24c896160a233f1f0fa2e52
Oleg Sokolov <klaus888@yandex.ru>
2023-06-03 14:47:51 +0300

    add form to card page

--- tipkor/poly/urls.py
+++ tipkor/poly/urls.py
@@ -4,8 +4,10 @@ from django.views.generic.base import TemplateView
 
 from .views import BookletView, CardView, LeafletView
 
+app_name = 'poly'
+
 urlpatterns = [
-    path('card/', CardView.as_view()),
+    path('card/', CardView.as_view(), name='card'),
     path('leaflet/', LeafletView.as_view()),
     path('booklet/', BookletView.as_view()),
 ]
