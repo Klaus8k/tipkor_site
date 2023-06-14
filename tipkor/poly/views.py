24d221066fd951398d96cf93e68a25a6822c1fc6
Oleg <klaus888@yandex.ru>
2023-06-15 00:26:39 +0300

    meta view form_data method add

--- tipkor/poly/views.py
+++ tipkor/poly/views.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 
 from django.http import HttpRequest, HttpResponse
-from django.urls.base import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormMixin
 
@@ -14,31 +13,44 @@ class PolyMeta(TemplateView, FormMixin):
     type_production = None
     form_class = None
     template_name = ''
-    success_url = reverse_lazy('')      
     model_class = None
-    del_keys = ['csrfmiddlewaretoken']
 
 
     def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
-        data_from_form = self.get_form_kwargs()['data'].dict() # TODO - вынести в метод извлечение данных из формы
+        """Если ПОСТ запрос, чистит данные из формы, добавляет в форму данные
+        Args:
+            request (HttpRequest): 
+        Returns:
+            HttpResponse: Передается в ГЕТ объекта, для расчета и отображения шаблона
+        """
+        self.data_form = self.get_form_data()
         context = super().get_context_data(**kwargs)
-        context['form'] = self.form_class(data_from_form)
-        return self.get(HttpRequest, *args, **kwargs)
+        context['form'] = self.form_class(self.data_form)
+        return self.get(request, *args, **kwargs)
 
     def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        """Если форма привязана - то расчет и добавление в контекст kwargs
+        Args:
+            request (HttpRequest): Свежий или из пост запроса
+        Returns:
+            HttpResponse: в супер класс обогащенный контекст для отображения в шаблоне
+        """
         if self.get_form().is_bound:
-            self.data_form = self.get_form_kwargs()['data'].dict() # TODO - вынести в метод извлечение данных из формы
-
-            self.data_form['type_production': self.type_production]
-            
-            del self.data_form['csrfmiddlewaretoken']
-            
-            # TODO try for 'no matches result'
-            result = {'result' : self.model_class.get_result(**self.data_form)} # TODO - приличный словарь для результата сделать. Что бы в шаблонах норм расставить
-            print(result['result'].cost)
-            kwargs.update(result)
+            result = self.model_class.get_result(**self.data_form)
+            kwargs.update({'result': result})
         return super().get(request, *args, **kwargs)
-
+    
+    def get_form_data(self) -> Dict:
+        """переводит данные из POST в словать
+        добавляет тип продукции и убирает токен
+        Returns:
+            Dict: словарь параметров из формы с типом продукции
+        """
+        form_data = self.request.POST.copy()
+        form_data.update({'type_production': self.type_production})
+        del form_data['csrfmiddlewaretoken']
+        return form_data.dict()
+        
     class Meta:
         abstract = True
 
@@ -47,18 +59,14 @@ class CardView(PolyMeta):
     type_production = 'card'
     form_class = Card_Form
     template_name = 'card.html'
-    success_url = reverse_lazy('poly:card')      
     model_class = Card_Model
-    del_keys = ['csrfmiddlewaretoken']
 
 
 class LeafletView(PolyMeta):
     type_production = 'leaflet'
     form_class = Leaflet_Form
     template_name = 'leaflet.html'
-    success_url = 'poly:leaflet'
     model_class = Leaflets_Model
-    del_keys = ['csrfmiddlewaretoken']
     
 
     
