2df995b5504c086ce209b9282139dd326e438d66
Oleg Sokolov <klaus888@yandex.ru>
2023-07-29 11:53:45 +0300

    1

--- tipkor/stamp/admin.py
+++ tipkor/stamp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Snap_item, Snap_type, Stamp, Stamp_type
+
 # Register your models here.
+
+admin.site.register([Snap_item, Snap_type, Stamp, Stamp_type])
--- tipkor/stamp/models.py
+++ tipkor/stamp/models.py
@@ -5,22 +5,29 @@ from django.db import models
 class Snap_type(models.Model):
     snap_type = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.snap_type
 
-class Stamp(models.Model):
+class Stamp_type(models.Model):
     type_stamp = models.CharField(choices=[('c_stamp', 'печать'),('r_stamp', 'штамп')], max_length=30)
-    express = models.BooleanField()
-    file = models.FileField()
-    comment = models.TextField(blank=True, max_length=100)
-    snap_type = models.ForeignKey(Snap_type, on_delete=models.DO_NOTHING)
-    count = models.IntegerField()
-    cost = models.IntegerField()
     
+    def __str__(self):
+        return self.type_stamp
     
 class Snap_item(models.Model):
     title = models.CharField(max_length=30)
-    type_stamp = models.ForeignKey(Stamp, on_delete=models.DO_NOTHING)
+    type_stamp = models.ForeignKey(Stamp_type, on_delete=models.DO_NOTHING)
     snap_type = models.ForeignKey(Snap_type, on_delete=models.DO_NOTHING)
     snap_img = models.ImageField()
     price = models.IntegerField()
+    
+class Stamp(models.Model):
+    type_stamp = models.ForeignKey(Stamp_type, on_delete=models.DO_NOTHING)
+    express = models.BooleanField()
+    file = models.FileField()
+    comment = models.TextField(blank=True, max_length=100)
+    snap = models.ForeignKey(Snap_item, on_delete=models.DO_NOTHING)
+    count = models.IntegerField()
+    cost = models.IntegerField()
     
     
