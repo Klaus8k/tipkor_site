f691fb6a34be59af9e0ad8e7d70f09eed4458987
Oleg Sokolov <klaus888@yandex.ru>
2023-08-17 11:48:03 +0300

    add sitemap

--- tipkor/tipkor/urls.py
+++ tipkor/tipkor/urls.py
@@ -7,10 +7,14 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('',TemplateView.as_view(template_name='tipkor.html')),
-    path('wide/', include('wide.urls')),
-    path('poly/', include('poly.urls')),
-    path('stamp/', include('stamp.urls')),
+    path('',TemplateView.as_view(template_name='tipkor.html'), name='tipkor'),
+    path('wide/', include('wide.urls'), name='wide'),
+    path('poly/', include('poly.urls'), name='poly'),
+    path('stamp/', include('stamp.urls'), name='stamp'),
+    
+    path('sitemap.xml',TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
+
+    
     path('expirement/', TemplateView.as_view(template_name='expirement.html')),
 ]
 
