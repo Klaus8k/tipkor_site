816452737babb5ca4f73a823fc8c88eb3df5d40f
Oleg <klaus888@yandex.ru>
2023-10-04 22:15:55 +0300

    1

--- tipkor/tipkor/views.py
+++ tipkor/tipkor/views.py
@@ -4,7 +4,9 @@ from django.views.generic import ListView
 
 
 def error_404_view(request, exeption):
-    return render(request, template_name='404.html')
+    response = render(request, template_name='404.html')
+    response.status_code = 404
+    return response
 
 def server_error(request):
     return render(request, template_name='404.html')
